81b9b5e316b1ca61c0e2fde8a0ef0ff9e407fe88
sucheng <943704372@qq.com>
2020-08-14 17:13:50 +0800

    原型

--- 10.js
+++ 10.js
@@ -23,4 +23,66 @@
 // var son = new Son(1,3)
 // son.subtract()
 // son.sum()
-var 
+// es6 常用功能
+// 1 函数的默认传值
+// function f1 (x, y = 1){
+//     console.log(x,y)
+// }
+// f1()
+// 2 解构的方式默认值 
+// function f2 ({x = 1, y = 3 } = {}){
+//     console.log(x,y)
+// }
+// f2({})
+// 箭头函数
+// var arr = [1,3,5,5]
+// arr.map(function(item){
+//     console.log(item)
+// })
+
+
+// console.log(arr.map((item) => item + 1))
+// console.log(arr)
+
+// 3 this 指向问题
+// function fn(){
+//     console.log('real',this)
+//     var arr = [1]
+//     arr.map(function(item){
+//         //普通函数指向全局对象 window 改变了指向也是不行
+//         console.log('普通函数',this)
+//     })
+//     arr.map(()=> {
+//         // this指向被改变了
+//         console.log('箭头函数',this)
+//     })
+// }
+// fn.call({a:100}) //改变this指向
+// 4 块级作用域
+// var obj = {name:1,age:2,sum:3}
+// var key
+// var arr = []
+// for(key in obj){
+//     arr.push(key)
+// }
+// console.log("67",key,arr)
+// for(let key1 in obj){
+//     console.log(key1)
+// }
+// console.log('71',key1) //会计作用域会报错了
+// 5 原型链添加
+// var obj = {
+//     css:function(){
+//         console.log('css')
+//     },
+//     htm:function(){
+//         console.log('htm')
+//     }
+// }
+// function Z (){
+//     console.log(111)
+// }
+// Z.prototype = obj
+// var  z = new Z();
+
+// z.htm()
--- es6/class/html.js
+++ es6/class/html.js
@@ -1,54 +1,56 @@
-function MathHanle(x,y){
-    this.x = x;
-    this.y = y
-}
-// 通过原型链添加方法
-MathHanle.prototype.add = function(){
-    return this.x + this.y
-}
-var math = new MathHanle(1,3)
-console.log(math.add())
+// function MathHanle(x,y){
+//     this.x = x;
+//     this.y = y
+// }
+// // 通过原型链添加方法
+// MathHanle.prototype.add = function(){
+//     return this.x + this.y
+// }
 
-console.log(typeof MathHanle) //function
-// 构造数数的显示原型中的构造器等于本省
-console.log(MathHanle.prototype.constructor === MathHanle) // true
-// 实例化对象的隐式原型 等于显示原型
-console.log(math.__proto__ === MathHanle.prototype)
-// 继承 --JS
-// 动物
-function Aniamll(){
-    this.eat = function(){
-        console.log('animall eat')
-    }
-}
-//狗
-function Dog(){
-    this.bark = function(){
-        console.log('dog bark')
-    }
-}
-// 原型链继承 动物方法
-Dog.prototype = new Aniamll()
-const d = new Dog()
-d.eat()
-// class 概念继承
-class Father{
-    constructor(name){
-        this.name = name
-    }
-    eat(){
-        console.log(this.name + '在吃东西')
-    }
-}
-class Son extends Father {
-    constructor(name){
-        super(name);
-        this.name = name
-    }
-    study(){
-        console.log(this.name + '在学习')
-    }
-}
-var s = new Son('son')
-s.eat()
-s.study()
+// var math = new MathHanle(1,3)
+// console.log(math.add())
+
+// console.log(typeof MathHanle) //function
+// // 构造数数的显示原型中的构造器等于本省
+// console.log(MathHanle.prototype.constructor === MathHanle) // true
+// // 实例化对象的隐式原型 等于显示原型
+// console.log(math.__proto__ === MathHanle.prototype)
+
+// // 继承 --JS
+// // 动物
+// function Aniamll(){
+//     this.eat = function(){
+//         console.log('animall eat')
+//     }
+// }
+// //狗
+// function Dog(){
+//     this.bark = function(){
+//         console.log('dog bark')
+//     }
+// }
+// // 原型链继承 动物方法
+// Dog.prototype = new Aniamll()
+// const d = new Dog()
+// d.eat() 
+// // class 概念继承
+// class Father{
+//     constructor(name){
+//         this.name = name
+//     }
+//     eat(){
+//         console.log(this.name + '在吃东西')
+//     }
+// }
+// class Son extends Father {
+//     constructor(name){
+//         super(name);
+//         this.name = name
+//     }
+//     study(){
+//         console.log(this.name + '在学习')
+//     }
+// }
+// var s = new Son('son')
+// s.eat()
+// s.study()
