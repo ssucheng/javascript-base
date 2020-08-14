6cc80b52d4c1370118edab37ac7811fa231c80e7
sucheng <943704372@qq.com>
2020-08-14 10:55:05 +0800

    普通构造函数继承和class方式继承

--- 10.js
+++ 10.js
@@ -1,25 +1,26 @@
-// 继承父类的方法
-class Father{
-    constructor(x,y){
-        this.x = x;
-        this.y = y
-    }
-    sum(){
-        console.log(this.x + this.y)
-    }
-}
-class Son extends Father{
-    constructor(x,y){
-        // super位置放在 this之前
-        super(x,y);
-        this.x = x;
-        this.y = y;
+// // 继承父类的方法
+// class Father{
+//     constructor(x,y){
+//         this.x = x;
+//         this.y = y
+//     }
+//     sum(){
+//         console.log(this.x + this.y)
+//     }
+// }
+// class Son extends Father{
+//     constructor(x,y){
+//         // super位置放在 this之前
+//         super(x,y);
+//         this.x = x;
+//         this.y = y;
 
-    }
-    subtract(){
-        console.log(this.x - this.y)
-    }
-}
-var son = new Son(1,3)
-son.subtract()
-son.sum()
+//     }
+//     subtract(){
+//         console.log(this.x - this.y)
+//     }
+// }
+// var son = new Son(1,3)
+// son.subtract()
+// son.sum()
+var 
--- es6/class/html.js
+++ es6/class/html.js
@@ -14,4 +14,41 @@ console.log(typeof MathHanle) //function
 console.log(MathHanle.prototype.constructor === MathHanle) // true
 // 实例化对象的隐式原型 等于显示原型
 console.log(math.__proto__ === MathHanle.prototype)
-
+// 继承 --JS
+// 动物
+function Aniamll(){
+    this.eat = function(){
+        console.log('animall eat')
+    }
+}
+//狗
+function Dog(){
+    this.bark = function(){
+        console.log('dog bark')
+    }
+}
+// 原型链继承 动物方法
+Dog.prototype = new Aniamll()
+const d = new Dog()
+d.eat()
+// class 概念继承
+class Father{
+    constructor(name){
+        this.name = name
+    }
+    eat(){
+        console.log(this.name + '在吃东西')
+    }
+}
+class Son extends Father {
+    constructor(name){
+        super(name);
+        this.name = name
+    }
+    study(){
+        console.log(this.name + '在学习')
+    }
+}
+var s = new Son('son')
+s.eat()
+s.study()
